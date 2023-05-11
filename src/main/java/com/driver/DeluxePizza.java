24cd948c471dbe45dd90da27ea8218d28aa096e4
svenkatesh0090 <venkateshcsp9@gmail.com>
2023-05-11 15:51:42 +0530

    Billing is proper

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,26 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
-
+    private int price;
+    private String bill;
+    private boolean extracheese=true;
+    private boolean extratoppings=true;
+    private boolean parcel;
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        this.parcel=false;
+        setExtracheese(true);
+        setToppings(true);
         // your code goes here
+        if(isVeg){
+            this.price=300+80+70;
+            this.bill=Integer.toString(this.price);
+        }
+        else{
+           this.price=400+80+120;
+            this.bill=Integer.toString(this.price);
+        }
+        setPrice(this.price);
+        setBill(this.bill);
     }
 }
